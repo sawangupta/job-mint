701eb4b41573d95eaec928530310aa5f3f1fa1bf
Sawan Gupta <sawan.gupta@flipkart.com>
2016-02-25 16:48:09 +0530

    Adding Basic Login

--- app/src/main/java/ekart/job_mint/LoginActivity.java
+++ app/src/main/java/ekart/job_mint/LoginActivity.java
@@ -3,6 +3,7 @@ package ekart.job_mint;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -184,9 +185,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
-            showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+            //showProgress(true);
+            Intent i = new Intent(this, MainActivity.class);
+            this.startActivity(i);
+            //mAuthTask = new UserLoginTask(email, password);
+            //mAuthTask.execute((Void) null);
         }
     }
 
